43b17baffa7e11b530ee8774fbeb4ac1921224ad
“MatchyChin” <cchi7170@uni.sydney.edu.au>
2023-10-27 11:37:24 +1100

    yellow background (fixed) update

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(700, 700);
+}
+
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
+
+function draw() {
+  background(225, 204, 0);
+}
